d5469472a90e89b5b6a5df01775f6793a816bb54
Gabriel Pampolha <pampolhag@gmail.com>
2021-07-06 18:57:37 -0300

    Delete pampobot directory
    cirei um repositório exclusivo para o pampobot atual

--- pampobot/functions/checkDM.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-
-const checkDM = (message = undefined, interaction = undefined) =>
-{
-    if (message && message.channel.type === 'dm') return true;
-    else if (interaction && interaction.member === undefined) return true;
-};
-
-module.exports = { checkDM };
